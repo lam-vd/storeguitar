beb569b9f32f0efeb0427695504badeb05cf4613
lamvd <vuduclam2412@gmail.com>
2024-03-21 22:24:42 +0700

    Edit code

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,2 +1,7 @@
 pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.turbo.min.js"
 pin "flowbite-datepicker", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/datepicker.turbo.min.js", preload: true
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
